e5ba328d717c6d53c3b6d7d271a9cebc63ab7a5d
mangbaujessica <mangbaujessica@hotmail.com>
2020-11-25 15:10:47 +0100

    updated data.js and script.js

--- testDePersonnalite/script.js
+++ testDePersonnalite/script.js
@@ -13,11 +13,15 @@ function render(arr) {
     form += `<h2>Question ${item.id}/8</h2>`;
     form += `<p>${item.question}</p>`;
     // iterer sur item.reponses
+    let reponses = '';
+    for (const reponse of arr.reponses) {
+      console.log('toto');
+      reponses += `<input type="radio" name="qst1" value="1" id="1" />${$reponse}.<br />`;
+    }
     form += '<input type="submit" value="Question suivante" />'; // ajouter un addeventlistener
     form += '</form>';
     form += '</div>';
   }
-
   // injecter dans le html
   mainHtml.innerHTML = form;
 }
