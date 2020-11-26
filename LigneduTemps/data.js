b0a36cc2dd468d6bdb3b2be219a16383bcfc95c2
Federica Buzzi <infofedericabuzzi@gmail.com>
2020-11-26 10:54:27 +0100

    ligne du temps

---
+++ LigneduTemps/data.js
@@ -0,0 +1,37 @@
+var bds = [
+  {
+    titre:
+      "Ada Lovelace (1815 - 1852) crée le premier algorithme de calcul de l’histoire",
+    expand:
+      "Elle rédige les premières esquisses de ce qui sera le langage informatique avec Charles Babbage, le concepteur de la Machine Analytique (un calculateur mécanique en somme)",
+  },
+  {
+    titre:
+      "Grace Hopper (1906 - 1992) fait partie de l’équipe qui conçoit l’ordinateur Harvard Mark I et collabore avec les équipes de Mark II et Mark III",
+    expand:
+      "Elle rédige les premières esquisses de ce qui sera le langage informatique avec Charles Babbage, le concepteur de la Machine Analytique (un calculateur mécanique en somme)",
+  },
+  {
+    titre:
+      "Jean E. Sammet (1928 - 2017) développe le langage informatique FORMAC en travaillant chez IBM",
+    expand:
+      "Elle collabore avec Grace Hopper et participe à la création du langage COBOL. Entre 1974 et 1976, Jean Sammet est présidente de la prestigieuse ACM (Association for Computing Machinery) qui rassemble les plus grands esprits des débuts des sciences de l’informatique",
+  },
+  {
+    titre:
+      "Karen Spärck Jones (1935 - 2007) est une pionnière dans le domaine de l’intelligence artificielle, qui travaille notamment sur l’analyse automatique du langage naturel",
+    expand:
+      "Elle étudie l’occurence des mots dans un texte pour en identifier automatiquement le contenu, le genre d’algorithme à l’origine des moteurs de recherche comme Google",
+  },
+  {
+    titre:
+      "Graphiste, Susan Kare (1954 - ) crée les premières polices Apple et les premières icônes Macintosh et Windows 3.0",
+    expand:
+      "Elle rejoint Apple en 1982 et réalise ses premières créations sur papier millimétré, un carré représentant un pixel. Susan Kare imagine les premières interfaces des systèmes d’exploitation",
+  },
+  {
+    titre: "Michelle Baker (1959 - ) est président de la Mozilla Foundation",
+    expand:
+      "Michelle est juriste, spécialisée en propriété intellectuelle, notamment dans le domaine des nouvelles technologies. Michelle est une figure majeure dans le mouvement Open Source et oeuvre pour l’ouverture et l’innovation sur le web.",
+  },
+];
