a36fb9ad3b5db414307be8f11700a6c8d2669591
fanny <fanny.wastelin@hotmail.com>
2020-11-27 10:50:23 +0100

    changement LigneduTemps

--- LigneduTemps/script.js
+++ LigneduTemps/script.js
@@ -1,15 +1,12 @@
 import './style.scss';
-import {bds} from'./data.js';
-// import $ from 'jquery';
+import $ from 'jquery';
+import { bds } from './data.js';
 // import axios from 'axios';
 
+// ${livre.resume.substr(0, 31)}
 
-//${livre.resume.substr(0, 31)}
-
-
-/*function afficheleTexte(textes) {
+/* function afficheleTexte(textes) {
     var contenu = ' ';
-    
 
     for (var texte of textes) {
         contenu += `<div class='box-${texte.id[0]}> ${texte.titre} <br>
@@ -23,72 +20,78 @@ import {bds} from'./data.js';
     var app = document.getElementById('boxun');
     app.innerHTML = data;
     */
-   function render(textes) {
-    const mainHtml = document.querySelector('main');
-    let form = '';
-    for (const texte of textes) {
-      form += `<div id="test-${texte.id}" class="textebd" >`;
-      form += `<h2>${texte.titre}</h2>`;
-      form += `<button id="btn">En savoir plus</button>`
-      form += `<p class="para" style="display:none">${texte.expand}</p>`;
-      form += '</div>';
-    }
-    mainHtml.innerHTML = form;
+function render(textes) {
+  const mainHtml = document.querySelector('main');
+  let form = '';
+  for (const texte of textes) {
+    form += `<div id="test-${texte.id}" class="textebd" >`;
+    form += `<h4>${texte.titre}</h4>`;
+    form += '<button id="btn">En savoir plus</button>';
+    form += `<p class="para" style="display:none">${texte.expand}</p>`;
+    form += '</div>';
   }
-  render(bds);
+  mainHtml.innerHTML = form;
+}
+render(bds);
 const paras = document.querySelectorAll('.para');
-  const btns= document.querySelectorAll('#btn');
+const btns = document.querySelectorAll('#btn');
+const text = $('#test-0');
+const text1 = $('#test-1');
+const text2 = $('#test-2');
+const text3 = $('#test-3');
+const text4 = $('#test-4');
+const text5 = $('#test-5');
 
-  btns[0].addEventListener("click",()=>{
-if (paras[0].style.display === 'none') {
-    paras[0].style.display ='block';
-}
-else{
-    paras[0].style.display ='none';
-}
+btns[0].addEventListener('click', () => {
+  if (paras[0].style.display === 'none') {
+    paras[0].style.display = 'block';
+   
+  } else {
+    paras[0].style.display = 'none';
+  }
 });
 
-btns[1].addEventListener("click",()=>{
-    if (paras[1].style.display === 'none') {
-        paras[1].style.display ='block';
-    }
-    else{
-        paras[1].style.display ='none';
-    }
-  });
+btns[1].addEventListener('click', () => {
+  if (paras[1].style.display === 'none') {
+    paras[1].style.display = 'block';
+  
+  } else {
+    paras[1].style.display = 'none';
+  }
+});
 
-btns[2].addEventListener("click",()=>{
-    if (paras[2].style.display === 'none') {
-        paras[2].style.display ='block';
-    }
-    else{
-        paras[2].style.display ='none';
-    }
-  });
+btns[2].addEventListener('click', () => {
+  if (paras[2].style.display === 'none') {
+    paras[2].style.display = 'block';
+  
+  } else {
+    paras[2].style.display = 'none';
+  }
+});
 
-btns[3].addEventListener("click",()=>{
-    if (paras[3].style.display === 'none') {
-        paras[3].style.display ='block';
-    }
-    else{
-        paras[3].style.display ='none';
-    }
-  });
+btns[3].addEventListener('click', () => {
+  if (paras[3].style.display === 'none') {
+    paras[3].style.display = 'block';
+   
+  } else {
+    paras[3].style.display = 'none';
+  }
+});
 
-btns[4].addEventListener("click",()=>{
-    if (paras[4].style.display === 'none') {
-        paras[4].style.display ='block';
-    }
-    else{
-        paras[4].style.display ='none';
-    }
-  });
+btns[4].addEventListener('click', () => {
+  if (paras[4].style.display === 'none') {
+    paras[4].style.display = 'block';
+    
+  } else {
+    paras[4].style.display = 'none';
+  }
+});
 
-btns[5].addEventListener("click",()=>{
-    if (paras[5].style.display === 'none') {
-        paras[5].style.display ='block';
-    }
-    else{
-        paras[5].style.display ='none';
-    }
-  });
+btns[5].addEventListener('click', () => {
+  if (paras[5].style.display === 'none') {
+    paras[5].style.display = 'block';
+    
+  } else {
+    paras[5].style.display = 'none';
+  }
+});
